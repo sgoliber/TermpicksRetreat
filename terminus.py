10bd7db42a9fddd97c508b3947f39f600c284db0
sgoliber <38142515+sgoliber@users.noreply.github.com>
2022-03-04 15:58:40 -0500

    add files via upload

--- terminus.py
+++ terminus.py
@@ -18,9 +18,10 @@ import geopandas as gpd
 
 
 
-
-
 def redistribute_vertices(geom, distance):
+    #INPUT: Linestring Geometry, distance between interpolated points
+    #OUTPUT: Linetring geometry with evenly spaced vertices along line 
+        
     if geom.geom_type == 'LineString':
         num_vert = int(round(geom.length / distance))
         if num_vert == 0:
@@ -40,109 +41,126 @@ def Average(lst):
     return sum(lst) / len(lst) 
 
 
-def near(centroid,centerline,valuedf = 'geometry'):
-    unary_union = centerline.unary_union
-    nearest = centerline['geometry']== nearest_points(centroid,unary_union)[1]
-    nearest_np = nearest.to_numpy()
-    value = np.where(nearest_np == True)[0][0]
-    id_num = centerline[valuedf].iloc[value]
 
-    return id_num
-
-
-
-class Terminus:
-    def __init__(self, dataframe,glacid):
-        self.data = dataframe[dataframe['GlacierID']==glacid]
-        self.glacid = glacid
+class termpicks_trace:
+    #INPUT: Termpicks Traces, GlacierID of interest
     
-    def points_along_trace(self,n_vert):
+    def __init__(self, dataframe,glacid):
+        #Subset terminus picks in TermPicks data by Glacier ID
+        
+        self.data = dataframe[dataframe['GlacierID']==glacid].copy()
+        self.glacid = glacid
+        
+    
+    #Interpolate evenly spaced points at redistibuted vertices along a line
+    def trace2points(self,n_vert=30):
         #Number of verticies to redistribute
 
         shpDF = self.data
         shpDF = shpDF.sort_values(by=['Date'])
 
-        #POINTS ALONG TRACE
+        #initalize list of points along geometry
         pointsList = []
+        #iterate over each terminus trace
         for i,r in shpDF.iterrows():
+            #Multi-line/split termini Multilinestrings
             if r.geometry.type == 'MultiLineString':
                 point_r = []
+                
+                #Split geometries in multiline string
                 exploded = r.explode()
                 segs = len(exploded.geometry)
+                
+                #for each segment, redistibute vertices
                 for i in range(0,segs):
-                    multiline_r = redistribute_vertices(exploded.geometry[i],150)
+                    multiline_r = redistribute_vertices(exploded.geometry[i],n_vert)
+                    #save each vertex as a coordiante
                     for coord in multiline_r.coords:
                         point_r.append(coord)
+                        
+                #turn list of coordiantes into a multipoint feature
                 points = MultiPoint(point_r)
                 pointsList.append(points)
 
             else:
-                multiline = r['geometry']
-                multiline_r = redistribute_vertices(multiline, n_vert)
+                #Same process as aobve, but for single Linestrings
+                singleline = r['geometry']
+                singleline_r = redistribute_vertices(singleline, n_vert)
                 point_r = []
-                for coord in multiline_r.coords:
+                for coord in singleline_r.coords:
                     point_r.append(coord)
                 points = MultiPoint(point_r)
                 pointsList.append(points)
             
     
-
+        #create new dataframe with points along geometry in place of 
+        #linestrings
+        
         shpDF['points'] = pointsList
         del shpDF['geometry']
-        shpDF = shpDF.rename(columns={"points": "geometry"})
-        shpDF.set_geometry(col='geometry', inplace=True)
+        terminus_points = shpDF.rename(columns={"points": "geometry"})
+        terminus_points.set_geometry(col='geometry', inplace=True)
         
-        return shpDF
+        return terminus_points
 
 
-class Centerline:
+class termpicks_centerline:
+    #INPUT: Termpicks Centerlines, GlacierID of interest
+    
     def __init__(self, dataframe ,glacid):
-        self.data = dataframe[dataframe['GlacierID']==glacid]
+        self.data = dataframe[dataframe['GlacierID']==glacid].copy()
         self.glacid = glacid
 
-    
+        #Interpolate evenly spaced points at redistibuted vertices along a
+        #centerline
     def line2points(self,n_vert):
         shpDF = self.data
         
-        #POINTS ALONG TRACE
-        pointsList = []
-        for i,r in shpDF.iterrows():
-            if r.geometry.type == 'MultiLineString':
-                point_r = []
-                exploded = r.explode()
-                segs = len(exploded.geometry)
-                for i in range(0,segs):
-                    multiline_r = redistribute_vertices(exploded.geometry[i],150)
-                    for coord in multiline_r.coords:
-                        point_r.append(coord)
-                points = MultiPoint(point_r)
-                pointsList.append(points)
-
-            else:
-                multiline = r['geometry']
-                multiline_r = redistribute_vertices(multiline, n_vert)
-                point_r = []
-                for coord in multiline_r.coords:
-                    point_r.append(coord)
-                points = MultiPoint(point_r)
-                pointsList.append(points)
-            
-        shpDF['points'] = pointsList
-        geos = [Point(p.x, p.y) for p in shpDF['points'].to_list()[0]]
-        center = gpd.GeoDataFrame(geometry = geos)
-        center['distance'] = center.distance(center.shift())
-        center['cumsum'] = center['distance'].cumsum()
+        #Get geometry of the centerline of intrest
+        singleline = shpDF['geometry'].iloc[0]
+        #Redistibute vertices
+        singleline_r = redistribute_vertices(singleline, n_vert)
+        point_r = []
+        #export each coordiate of the redistributed vertices 
+        for coord in singleline_r.coords:
+            point_r.append(coord)
+        points = MultiPoint(point_r)
+    
+        #save each x,y as an individual point per row
+        geos = [Point(p.x, p.y) for p in points]
+        centerline_points = gpd.GeoDataFrame(geometry = geos, crs="EPSG:3413")
         
-        return center
+        #calculate distance between points and save as distance column
+        centerline_points['distance'] = centerline_points.distance(centerline_points.shift())
+        #calculate cumsum of distances between points and save as cumsum column
+        centerline_points['cumsum'] = centerline_points['distance'].cumsum()
+        
+        return centerline_points
         
     
     
-class CalcRetreat:
+class termpicks_interpolation:
+    #INPUT: output from termpicks_points, output from termpicks_centerline
+        
     def __init__(self, terminus_points, centerline_points):
         self.termini = terminus_points
         self.centerline = centerline_points
         self.count = len(terminus_points)
+        
+    #Find nearest point to a dataframe of points
+    def near(self,centroid,centerline_points,valuedf):
+        #INPUT: point, centerline with interpolted points 
+        #OUTPUT
+        
+        unary_union = centerline_points.unary_union
+        nearest = centerline_points['geometry']== nearest_points(centroid,unary_union)[1]
+        nearest_np = nearest.to_numpy()
+        value = np.where(nearest_np == True)[0][0]
+        id_num = centerline_points[valuedf].iloc[value]
+
+        return id_num
     
+    #Find the mean trace location of a terminus 
     def mean_trace_loc(self):
         
         meanlocs = []
@@ -152,7 +170,8 @@ class CalcRetreat:
         for line in geoms:
             allpoints = []
             for point in line:
-                pp = near(point,self.centerline,valuedf='geometry')
+                val = 'geometry'
+                pp = self.near(point,self.centerline,val)
                 allpoints.append(pp)
         
             ax=[]
@@ -172,14 +191,17 @@ class CalcRetreat:
         
         return  newDF   
     
-
+    #Get the closest point in the centerline to the mean location
+    #Calculate the retreat from the cumulative distance along the terminus
+    #Date determines what the inital '0' pick is, otherwse its the earliest 
+    #avalible pick
     def calc_retreat(self):
         
         newDF = self.mean_trace_loc()
         
         nearestCL = []
         for point in newDF['geometry']:
-            pp = near(point,self.centerline,valuedf='cumsum')
+            pp = self.near(point,self.centerline,valuedf='cumsum')
             nearestCL.append(pp)
     
         #CALCULATE THE RETREAT
---
+++ termpicks_tutorial.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  3 15:19:44 2022
+
+@author: sofyg
+"""
+import geopandas as gpd
+from terminus import termpicks_trace, termpicks_centerline, termpicks_interpolation
+
+termpicks_df = gpd.read_file('TermPicks+CALFIN_V2.shp')
+centerlines_df = gpd.read_file('termpicks_centerlines/termpicks_centerlines.shp')
+
+
+trace_points = termpicks_trace(termpicks_df,2).trace2points(n_vert = 30)
+centerline_points = termpicks_centerline(centerlines_df,2).line2points(n_vert = 30)
+
+
+retreat = termpicks_interpolation(trace_points,centerline_points).calc_retreat()
+mean_locations = termpicks_interpolation().mean_trace_loc()
